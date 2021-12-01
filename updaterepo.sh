2382be63c0d2847edd4a2ba79ee0aed785af9a28
Smartx1 <92438460+Smartx1@users.noreply.github.com>
2021-12-01 23:44:26 +0400

    Delete updaterepo.sh

--- updaterepo.sh
+++
@@ -1,9 +0,0 @@
-rm Packages Packages.bz2 Release
-
-	apt-ftparchive packages ./dobs > Packages
-
-	bzip2 -c9 Packages > Packages.bz2
-
-	apt-ftparchive release -c ./assets/repo/repo.conf . > Release
-
-echo "Done!"
